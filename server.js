ece1258fc33fa45e30606f3f7cdee5d770a82d1b
DepechCode <marty.j.six@gmail.com>
2020-08-07 11:43:27 -0400

    juj

--- server.js
+++ server.js
@@ -8,11 +8,9 @@ const helmet = require('helmet');
 const path = require('path');
 
 const weatherRouter = require('./routes/weather');
-const geocoding = require('./routes/geocoder');
 const shopRouter = require('./routes/shop-totals');
 const cutterRouter = require('./routes/cutter-totals');
 const sawRouter = require('./routes/saw-totals');
-const crewRouter = require('./routes/crews');
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -22,11 +20,9 @@ app.use(helmet());
 app.use(morgan('common'));
 
 app.use('/api/weather', weatherRouter);
-app.use('/api/geocode', geocoding);
 app.use('/api/shop', shopRouter);
 app.use('/api/cutter', cutterRouter);
 app.use('/api/saw', sawRouter);
-app.use('/api/crews', crewRouter);
 
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('client/build'));
